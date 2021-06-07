f594d198c5349f05d78762c8bdd3068c209d2ae0
Taras Konovalov <taras.konovalov2014@gmail.com>
2021-06-07 14:39:01 +0300

    Updates

---
+++ precache-manifest.320c79d03a5ffb57f2d4c1777fbecf02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb4b63dee01710dd1634f6fac35a1ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1d051dfcc0c525bb4ec",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "a1fcdca2e8d9401c9acf",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "d1d051dfcc0c525bb4ec",
+    "url": "./static/js/2.28065652.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.28065652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1fcdca2e8d9401c9acf",
+    "url": "./static/js/main.6472ca3c.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
